5eb5cb48a9357a8bf0e6deb322cf40a7e1bb431b
Andy Crowe <38168099+crarem@users.noreply.github.com>
2022-12-05 08:34:02 +1100

    added ability to exclude certain pairs
    e.g. if two members are husband and wife etc.

--- secret-santa.py
+++ secret-santa.py
@@ -3,13 +3,31 @@ import random
 # List of names for secret santa
 names = ["Jane", "John", "Sue", "Bob", "Molly", "Tom"]
 
-# Shuffle the list of names
-random.shuffle(names)
+# List of exclusions, where each element is a tuple containing the names of
+# two parties that cannot give to each other
+exclusions = [("Jane", "Bob"), ("John", "Sue"), ("Molly", "Tom")]
 
-# Assign each person in the list to give to the next person in the list,
-# except for the last person, who will give to the first person
-givers = names[1:] + [names[0]]
+# Loop until a valid assignment is found
+while True:
+  # Shuffle the list of names
+  print('Shuffling names...')
+  random.shuffle(names)
 
-# Print the list of givers and use zip function to group the recipients alongside givers
-for giver, recipient in zip(names, givers):
-  print(f"{giver} will give to {recipient}")
+  # Assign each person in the list to give to the next person in the list,
+  # except for the last person, who will give to the first person
+  givers = names[1:] + [names[0]]
+
+  # Check if the current assignment violates any of the exclusions
+  valid = True
+  for exclusion in exclusions:
+    if ((names[names.index(exclusion[0])], givers[names.index(exclusion[0])]) == exclusion or (names[names.index(exclusion[1])], givers[names.index(exclusion[1])]) == (exclusion[1],exclusion[0])):
+      valid = False
+      print('Exclusion found!', exclusion)
+      break
+
+  # If the current assignment is valid, print the list of givers and recipients and exit the loop
+  if valid:
+    for giver, recipient in zip(names, givers):
+      print(f"{giver} will give to {recipient}")
+    break
+
